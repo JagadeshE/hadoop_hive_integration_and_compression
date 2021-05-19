750970257cfb85ae2c56524455abccee2d06fb1c
JagadeshE <jagadeshkumar1893@gmail.com>
2021-05-19 12:02:02 +0530

    Update hadoop_hive_compression.java

--- hadoop_hive_compression.java
+++ hadoop_hive_compression.java
@@ -71,11 +71,8 @@ public class hadoop_hive_compression {
 			System.out.print("Compression time is: ");
 
 			System.out.println(
-//                difference_In_Years + " years, "
-//                + difference_In_Days + " days, " 
-//                +
-					difference_In_Hours + " hours, " + difference_In_Minutes + " minutes, " + difference_In_Seconds
-							+ " seconds, " + difference_In_milliSeconds + " milliseconds");
+				difference_In_Hours + " hours, " + difference_In_Minutes + " minutes, " + difference_In_Seconds
+				+ " seconds, " + difference_In_milliSeconds + " milliseconds");
 		}
 
 		// Catch the Exception
@@ -105,19 +102,16 @@ public class hadoop_hive_compression {
 			String intput_file_path = input_sc.nextLine(); // reads string.
 
 			File f = new File(intput_file_path);
-	        long fileSize = f.length();
+		        long fileSize = f.length();
 			
 			// Input file - local file system
-//		   in = new BufferedInputStream(new FileInputStream("/home/jagadesh/hadoop_data/log.txt"));
 			in = new BufferedInputStream(new FileInputStream(intput_file_path));
 
 			// Output file path in HDFS
-
 			Scanner output_sc = new Scanner(System.in); // System.in is a standard input stream.
 			System.out.print("Enter a Compression File Saved Path with file name in HDFS: ");
 			String saved_file_path = output_sc.nextLine(); // reads string.
 
-//		   Path outFile = new Path("/home/hadoop/hadoop/files/Zstd_out/test.zst");
 			Path outFile = new Path(saved_file_path);
 
 			// Verifying if the output file already exists
@@ -151,10 +145,10 @@ public class hadoop_hive_compression {
 				Afterdate = sdfDate2.format(now2);
 				findDifference(beforedate, Afterdate);
 
-                // PRINT FILE SIZE OF BEFORE AND AFTER COMPRESSION 
+		                // PRINT FILE SIZE OF BEFORE AND AFTER COMPRESSION 
 				Configuration config = new Configuration();
 
-//				// Get the filesystem - HDFS
+				// Get the filesystem - HDFS
 				FileSystem file_sys = FileSystem.get(config);
 				
 				long output_file_size = file_sys.getContentSummary(outFile).getSpaceConsumed();
@@ -171,107 +165,65 @@ public class hadoop_hive_compression {
 				String Input_file_name = intput_file_path;
 				if(intput_file_path.indexOf("/") != -1) {
 					Input_file_name = intput_file_path.substring(intput_file_path.lastIndexOf("/") + 1);
-//					System.out.println("INPUT FILE NAME :" + " " + Input_file_name);
-
 				}
 				System.out.println("INPUT FILE NAME :" + " " + Input_file_name);
 				
 				String Output_file_name = saved_file_path;
 				if(saved_file_path.indexOf("/") != -1) {
 					Output_file_name = saved_file_path.substring(saved_file_path.lastIndexOf("/") + 1);
-//					System.out.println("OUTPUT FILE NAME :" + " " + Output_file_name);
-
 				}
 				System.out.println("OUTPUT FILE NAME :" + " " + Output_file_name);
 
-				
-//				FSDataInputStream input = null;
-//
-//				try {
-//					// Open the path mentioned in HDFS
-//					input = file_sys.open(new Path(uri));
-//					IOUtils.copyBytes(input, System.out, 4096, false);
-//
-//					System.out.println("End Of file: HDFS file read complete");
-//
-//				} finally {
-//					IOUtils.closeStream(input);
-//				}
-//				
-				
 								
 				// Register driver and create driver instance
-		      try {
-			     Class.forName(driverName);
-		      }
-		      catch(ClassNotFoundException ex){
-		    	  System.out.println("DRIVER CLASS NOT FOUND .....");
-		      }
+		      		try {
+			     		Class.forName(driverName);
+		      		}
+		      		catch(ClassNotFoundException ex){
+		    	  		System.out.println("DRIVER CLASS NOT FOUND .....");
+		     		}
 
 
-			  // get connection
-//			  Connection con = DriverManager.getConnection("jdbc:hive2://", "", "");
-			  Connection con = DriverManager.getConnection("jdbc:hive2://localhost:10000/default", "", "");
-			  Statement stmt = (Statement) con.createStatement();
-//			  ResultSet test = stmt.executeQuery("CREATE DATABASE userdb2");
-//			  System.out.println("Database userdb created successfully.");
+			  	// get connection
+			  	Connection con = DriverManager.getConnection("jdbc:hive2://localhost:10000/default", "", "");
+			  	Statement stmt = (Statement) con.createStatement();
 			  
-
-		      try {
-			      boolean create_database = stmt.execute("CREATE DATABASE IF NOT EXISTS compression_datas");
-//			      ResultSet create_database = stmt.executeQuery("CREATE DATABASE IF NOT EXISTS compression_datas");
-			      System.out.println("Database compression_datas created successfully.");
-			      }catch (Exception exception){
-			    	  exception.printStackTrace();
-			      }
+		      		try {
+			      		boolean create_database = stmt.execute("CREATE DATABASE IF NOT EXISTS compression_datas");
+			      		System.out.println("Database compression_datas created successfully.");
+			      	}catch (Exception exception){
+			    	  	exception.printStackTrace();
+			      	}
 			      
-			  try {
-				  boolean create_table = stmt.execute("CREATE TABLE IF NOT EXISTS "
-			    	      +" compression_datas.compression_file_details ( id INT, file_name STRING, "
-			    	      +" file_path STRING, file_size BIGINT)stored as ORC TBLPROPERTIES ('transaction'='true')");
+			  	try {
+				  	boolean create_table = stmt.execute("CREATE TABLE IF NOT EXISTS "
+			    	      	+" compression_datas.compression_file_details ( id INT, file_name STRING, "
+			    	      	+" file_path STRING, file_size BIGINT)stored as ORC TBLPROPERTIES ('transaction'='true')");
 			      
-			      System.out.println("File Details Table created." + create_table);
-			      }catch (Exception exception){
-			    	  exception.printStackTrace();
-			      }	  
+	// 			      System.out.println("File Details Table created." + create_table);
+			      	}catch (Exception exception){
+			    	  	exception.printStackTrace();
+			      	}	  
 			  
-//			  boolean check_table_ispresent = stmt.execute("Select * from compression_datas.compression_file_details");
-//			  System.out.println("CHECK TABLE :"+ check_table_ispresent);
-//			 
-//			  
-//			  boolean check_table_present = stmt.execute("SHOW TABLES IN compression_datas LIKE 'compression_file'");
-//			  System.out.println("CHECK TABLE IS PRESENT OR NOT :"+ check_table_present);
-//
-//			  
-//			  ResultSet check_table = stmt.executeQuery("Select * from compression_datas.compression_file_details");
-//			  System.out.println("CHECK TABLE :"+ check_table);
-//			  if(check_table != null){
-//				  System.out.println(" TABLE CREATED");
-//				  }
-//			  
-			  try {
-				  boolean data_insert = stmt.execute("INSERT INTO table "
+			  	try {
+				  	boolean data_insert = stmt.execute("INSERT INTO table "
 					  + " compression_datas.compression_file_details VALUES "
-					  + "(2,"+"'"+Output_file_name +"'"+','+"'"+ saved_file_path +"'"+ ','+ output_file_size +")" );					  
-					  
-			  }catch (Exception exception){
-		    	  exception.printStackTrace();
-		      }
+					  + "(2,"+"'"+Output_file_name +"'"+','+"'"+ saved_file_path +"'"+ ','+ output_file_size +")" );					  	  
+			 	}catch (Exception exception){
+		    	  		exception.printStackTrace();
+		      		}
 			  
-			  ResultSet res = stmt.executeQuery("select * from compression_datas.compression_file_details");
-			  System.out.println("TABLE DATAS :"+ res);
+// 			  	ResultSet res = stmt.executeQuery("select * from compression_datas.compression_file_details");
+// 			  	System.out.println("TABLE DATAS :"+ res);
 
-//			  String sql= "SHOW TABLES compression_file_details";
-//			  System.out.println("SHOW TABLE :"+ sql);
-//			  ResultSet resut=stmt.executeQuery(sql);
-			  
-			  con.close();
-
+//			  	String sql= "SHOW TABLES compression_file_details";
+//			  	ResultSet resut=stmt.executeQuery(sql);
+				
+			  	con.close();
 			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-	
+	}	
 }
